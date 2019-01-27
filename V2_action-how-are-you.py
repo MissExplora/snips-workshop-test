0c6dcd626a3949559191da8d0d321214dce39fb2
Dora Budic <dora.budic@ericsson.com>
2019-01-27 15:55:42 +0100

    Test2

--- V2_action-how-are-you.py
+++ V2_action-how-are-you.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 from hermes_python.hermes import Hermes
 
-INTENT_HOW_ARE_YOU = "bezzam:how_are_you"
-INTENT_GOOD = "bezzam:feeling_good"
-INTENT_BAD = "bezzam:feeling_bad"
-INTENT_ALRIGHT = "bezzam:feeling_alright"
+INTENT_HOW_ARE_YOU = "missexplora:how_are_you"
+INTENT_GOOD = "missexplora:feeling_good"
+#INTENT_BAD = "bezzam:feeling_bad"
+#INTENT_ALRIGHT = "bezzam:feeling_alright"
 
-INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_ALRIGHT]
+#INTENT_FILTER_FEELING = [INTENT_GOOD, INTENT_BAD, INTENT_ALRIGHT]
+INTENT_FILTER_FEELING = [INTENT_GOOD]
 
 
 def main():
     with Hermes("localhost:1883") as h:
         h.subscribe_intent(INTENT_HOW_ARE_YOU, how_are_you_callback) \
          .subscribe_intent(INTENT_GOOD, feeling_good_callback) \
-         .subscribe_intent(INTENT_BAD, feeling_bad_callback) \
-         .subscribe_intent(INTENT_ALRIGHT, feeling_alright_callback) \
+         #.subscribe_intent(INTENT_BAD, feeling_bad_callback) \
+         #.subscribe_intent(INTENT_ALRIGHT, feeling_alright_callback) \
          .start()
 
 
